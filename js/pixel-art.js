2a4599847ad6ce63059c50d65a3c0bdb9d5d25d3
aleceliavane32 <alejandra_bravo31@yahoo.com.ar>
2017-11-06 21:58:28 -0300

    nue
    vari

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -1,29 +1,42 @@
 var nombreColores = ['White', 'LightYellow',
-  'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
-  'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
-  'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
-  'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
-  'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
-  'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
-  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
-  'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
-  'LightGreen', 'PaleGreen', 'PaleTurquoise',
-  'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
-  'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
-  'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
-  'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
-  'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
-  'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
-  'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
-  'BlueViolet', 'DarkViolet', 'DarkOrchid',
-  'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
-  'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
+'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
+'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
+'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
+'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
+'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
+'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
+'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
+'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
+'LightGreen', 'PaleGreen', 'PaleTurquoise',
+'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
+'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
+'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
+'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
+'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
+'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
+'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
+'BlueViolet', 'DarkViolet', 'DarkOrchid',
+'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
+'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
 ];
+
+//son Variable que vay a utilizar durante el desarrollo de la tarea.
 
 var  $paleta = $("#paleta");
 var  $grilla-pixeles = $("#grilla-pixeles");
+var $idColor= $('indicador-de-color');
+var $idColorMsj= $('#indicador-de-color-mensaje');
+var $grDePix =$('#grilla-pixeles');
+// esta funcion es para reutiliza la grilla en diferentes cituaciones.
 
-function genPaleColor(){
+function cambiandoIdPixel(color){
+  $idColor.css('background-color', 'color');
+  $idColorMsj.text('lapiz: ${color}');
+}
+
+// es la function de recorre la paleta de nombreColores.
+
+function genPaleColor(event){
   for (var myHis = 0; myHis < nombreColores.length; myHis++) {
     var $i = $(<'div/'>, {'class': 'color-paleta'});
 
@@ -31,15 +44,22 @@ function genPaleColor(){
     $i.css('background-color', myHis);
   }
 }
+
+// es la function que recorre la grilla-pixeles 1749.
+
 function griPixel(){
   for (var i =0; i < 1749; i++) {
     var $and =$(<'div/'>, {'class': 'grilla-pixeles'});
+    $grilla
   }
 }
+// es la funcion de clic.
 
-function cliClolor(){
-
+function clicEnClolor(){
+  nuevoColor = $(event.target).css('background-color');
+  colorNuevo(nuevoColor);
 }
+
 // Variable jQuery para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
 var $colorPersonalizado = $('#color-personalizado');
